3b70f20f256b9d3eaece82934638ab5466f6917f
ezehlivinus <ezehlivinusezeh@gmail.com>
2022-08-16 18:30:49 +0100

    update

---
+++ introduction-to-programming/challenges.js
@@ -0,0 +1,13 @@
+const numbers = [2];
+numbers = 2
+
+let sum = 0;
+for (let number of numbers) {
+  sum = sum + number
+  // console.log(`The sum is ${sum} ${ 2 + 4} and number is ${number}`)
+}
+
+console.log(sum)
+
+const _3ToPower6 = Math.pow(3, 2)
+console.log(Math.abs(-1))
---
+++ js-fundamentals/function.js
@@ -0,0 +1,35 @@
+function printName() {
+  console.log('Ezeh Livinus')
+}
+
+// invoke/call the function
+printName()
+printName()
+printName()
+
+const square = (base, power) => {
+  console.log(base ** power)
+}
+
+// square(2, 4)
+// square(6, 2)
+// square(3, 3)
+
+const add = function(a, b) {
+  return a + b
+}
+
+const result = add(2, 3)
+console.log(result)
+console.log(add(10, result))
+
+/**
+ * Take away: create a function that takes firstName 
+ * and lastName as arguments and returns the full name
+ * 
+ * Difference between methods and functions
+ * 
+ * Create an object call cat that has at least 3 methods
+ * 
+ * 3 methods each for an array, string and object
+ */
